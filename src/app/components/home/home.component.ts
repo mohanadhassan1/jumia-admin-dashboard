3048e1cfb19de174cbded7ed51957afab2d47fbe
Eman Hamed <emanhamed270@gmail.com>
2024-04-01 14:29:08 +0200

    home2

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,12 +3,13 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
 import { RatingComponent } from './rating/rating.component';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule,MatChipsModule,RatingComponent],
+  imports: [MatCardModule, MatButtonModule,MatChipsModule,RatingComponent,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
