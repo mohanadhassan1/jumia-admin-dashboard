a2d5ea697be6c204e40c9cb93ea2da044180c6aa
Eman Hamed <emanhamed270@gmail.com>
2024-04-03 12:16:42 +0200

    home-edit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
 import { RatingComponent } from './rating/rating.component';
 import { RouterLink } from '@angular/router';
+import { jwtDecode } from "jwt-decode";
+import { __values } from 'tslib';
+
+
 
 
 @Component({
@@ -13,6 +17,26 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  decodedToken: any;
+  s:any;
+  token:any
+  tokObject:any
+  tok:any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.token=localStorage.getItem("token")
+     this.tokObject = JSON.parse(this.token);
+     this.tok =this.tokObject.token;    
+     console.log(this.tok);
+    try {
+      this.decodedToken = jwtDecode(this.tok) // Pass tok variable to jwt_decode
+      console.log(this.decodedToken);
+    } catch (error) {
+      console.error('Error decoding token:', error);
+    }
+  }
 
 }
